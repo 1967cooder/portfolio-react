f004e05092fcc05f5f581374f3310b9f56ac16e6
1967cooder <silvanalindholm@hotmail.com>
2026-01-08 01:02:24 +0200

    Update Projects component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CategoryContainer from "./CategoryContainer";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Projects = () => {
   const [categories, setCategories] = useState([]);
@@ -16,21 +16,38 @@ const Projects = () => {
   }, []);
 
   return (
-    <Box
-      sx={{
-        p: 3,
-        display: "grid",
-        gridTemplateColumns: {
-          xs: "1fr", // mobile
-          sm: "repeat(2, 1fr)", // tablet
-          md: "repeat(3, 1fr)", // desktop
-        },
-        gap: 3,
-      }}
-    >
-      {categories.map((category) => (
-        <CategoryContainer key={category} category={category} />
-      ))}
+    <Box sx={{ p: 3 }}>
+      {/* 🔹 Title */}
+      <Typography
+        variant="h4"
+        sx={{
+          mb: 3,
+          textAlign: "left",
+          background: "linear-gradient(90deg, #00ffea, #7c4dff)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          //   fontWeight: 700,
+        }}
+      >
+        Projects
+      </Typography>
+
+      <Box
+        sx={{
+          p: 3,
+          display: "grid",
+          gridTemplateColumns: {
+            xs: "1fr", // mobile
+            sm: "repeat(2, 1fr)", // tablet
+            md: "repeat(3, 1fr)", // desktop
+          },
+          gap: 3,
+        }}
+      >
+        {categories.map((category) => (
+          <CategoryContainer key={category} category={category} />
+        ))}
+      </Box>
     </Box>
   );
 };
